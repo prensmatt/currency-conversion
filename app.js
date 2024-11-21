14d0f53673502f475255f95b46d1ac0fa08d827f
QadirzadeGultac <gultac.qadirzade@mail.ru>
2024-11-21 14:52:01 +0400

    refresh

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ let toCurrency = 'USD';
 let error = document.querySelector(".internet");
 let firstInp = document.querySelector(".input-one input");
 let secondInp = document.querySelector(".input-two input");
+let burgerMenu = document.querySelector(".burger-menu");
+let listBurger = document.querySelector(".list-burger");
+burgerMenu.addEventListener("touchstart",()=>{
+    listBurger.classList.toggle("disnone");
+})
 let rate = 1;
 let rate1 = 1;
 let a;
@@ -41,8 +46,9 @@ function updateExchangeRate() {
             else if (a == 1) {
                 secondInp.value = (firstInp.value * rate).toFixed(5);
             }
-            error.classList.add('disnone')
+
         })
+        error.classList.add('disnone')
 }
 function wifi() {
     while (!navigator.onLine) {
